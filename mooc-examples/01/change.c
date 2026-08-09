d475abb865cfcda6f2143e07977d96c848340a6c
peng24040330 <3200835986@qq.com>
2026-08-09 11:12:37 +0800

    增加常量

--- mooc-examples/01/change.c
+++ mooc-examples/01/change.c
@@ -2,10 +2,11 @@
 int main()
 {
     int price=0;
+    const int amount =100;
     printf("请输入金额（元）");
     scanf("%d",&price);
     
-    int change = 100-price;
+    int change = amount-price;
     printf("找您%d元\n",change);
 
     return 0;
