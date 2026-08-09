f356adfb2d0824d621af59e7a281ca03a9d3ee71
peng24040330 <3200835986@qq.com>
2026-08-09 23:42:58 +0800

    commit

---
+++ luogu-basic/shunxujiegou/P5708/P5708.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double a,b,c,p,s;
+    scanf("%lf %lf %lf",&a,&b,&c);
+    p=(a+b+c)/2.0;
+    s=sqrt(p*(p-a)*(p-b)*(p-c));
+    printf("%.1f",s);
+
+    return 0;
+    
+}
